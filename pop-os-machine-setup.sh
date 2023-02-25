66e8e7c4584740c6303cfed441a39a86ee7f8123
qcpi069 <qcpi069@pop-os.localdomain>
2023-02-25 16:22:36 -0600

    adds docker and dbeaver installs to pop-os script

--- pop-os-machine-setup.sh
+++ pop-os-machine-setup.sh
@@ -60,4 +60,7 @@ newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-
+# Install DBeaver
+sudo  wget -O /usr/share/keyrings/dbeaver.gpg.key https://dbeaver.io/debs/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
