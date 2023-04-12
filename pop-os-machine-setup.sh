1e58ec25c15f50557e5e69e20dd35447339ef3b5
qcpi069 <jose.a.deguzman@gmail.com>
2023-04-11 23:22:01 -0500

    removes error with docker group

--- pop-os-machine-setup.sh
+++ pop-os-machine-setup.sh
@@ -52,7 +52,6 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
